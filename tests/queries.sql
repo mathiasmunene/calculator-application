0ce9a503a5415b4a7a0c07d756da27dd4c27b784
mathiasmunene <mathiasmunene@gmail.com>
2025-06-26 21:18:03 +0300

    sql final

--- tests/queries.sql
+++ tests/queries.sql
@@ -1,8 +1,15 @@
 CREATE TABLE personal_info(
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER  PRIMARY KEY AUTOINCREMENT,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     middle_name VARCHAR(50),
     age INTEGER NOT NULL,
-    email TEXT NOT NULL UNIQUE KEY,
-)
+    email TEXT NOT NULL UNIQUE
+);
+
+INSERT INTO personal_info (first_name, last_name, middle_name, age, email)
+VALUES ('John', 'Doe', 'Michael', 30, 'john.doe@example.com'),
+       ('Jane', 'Smith', 'Ann', 25, 'jane.smith@example.com'),
+       ('Emily', 'Davis', NULL, 22, 'emily.davis@example.com'); 
+
+SELECT * FROM personal_info;
