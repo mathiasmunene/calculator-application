08b71c155cf72cb7b77f3bd16ca1ac5c29610213
mathiasmunene <mathiasmunene@gmail.com>
2025-06-26 18:45:35 +0300

    works

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from lib.calculator import sum, minus, multiply, divide 
+
+
 def main():
     print("Welcome to the CLI Calculator!")
 
@@ -10,7 +13,24 @@ def main():
             try:
                 a = float(input("Enter first number: "))
                 b = float(input("Enter second number: "))
+                if choice == '1':                    
+                    result = sum(a, b)
+                    print(f"{a} + {b} = {result}")
+                elif choice == '2':
+                    result = minus(a, b)
+                    print(f"{a} - {b} = {result}")
+                elif choice == '3':
+                    result = multiply(a, b)
+                    print(f"{a} * {b} = {result}")
+                elif choice == '4':
+                    result = divide(a, b)
+                    print(f"{a} / {b} = {result}")
+            except ValueError as e:
+                print("Invalid input:", e)
         elif choice == '5':
             break
         else:
             print("Invalid Input:", choice)
+
+if __name__ == "__main__":
+    main()
